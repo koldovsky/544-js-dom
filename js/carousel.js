91fc6cc80a68151bcd25c89a3515afd230baf3f7
Vyacheslav Koldovskyy <koldovsky@koldovsky.com>
2020-11-20 18:52:41 +0000

    Added carousel

---
+++ js/carousel.js
@@ -0,0 +1,20 @@
+const images = [
+    'img/baby-yoda.svg',
+    'img/banana.svg',
+    'img/girl.svg',
+    'img/viking.svg',
+]
+let currentIdx = 0;
+function slide() {
+    currentIdx = ++currentIdx > images.length - 1 ? 0 : currentIdx;
+    showSlide(currentIdx);
+}
+setInterval(slide, 3000);
+function back() {
+    currentIdx = --currentIdx < 0 ? images.length - 1 : currentIdx;
+    showSlide(currentIdx);
+}
+document.querySelector('.carousel .btn-back').addEventListener('click', back);
+function showSlide(idx) {
+    document.querySelector('.carousel img').src = images[idx];
+}
